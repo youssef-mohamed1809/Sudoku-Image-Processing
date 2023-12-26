e569bc646ae82d37d8f5e35e76167d2ad88bb766
Youssef <youssef.mohamed1809@gmail.com>
2023-12-26 15:30:13 +0200

    Added closing in Phase  + redo some images

--- intersections.py
+++ intersections.py
@@ -3,78 +3,90 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+def calculate_intersections(path):
+    img1 = cv2.imread(path)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
 
+    x = img1.shape[0]
+    y = img1.shape[1]
+
+    ## To draw horizontal lines
+    halfy = y // 2
+
+    l1Start = (0, halfy)
+    l1End = (x, halfy)
+
+    quartery = halfy // 2
+    l2Start = (0, quartery)
+    l2End = (x, quartery)
+
+    l3Start = (0, halfy + quartery)
+    l3End = (x, halfy + quartery)
+
+    image = np.zeros_like(img1, dtype = np.uint8)
+
+    image = cv2.line(image, (0, 18), (x, 18), (255, 255, 255), 1)
+    image = cv2.line(image, (0, 35), (x, 35), (255, 255, 255), 1)
+    image = cv2.line(image, (0, 53), (x, 53), (255, 255, 255), 1)
+    image = cv2.line(image, (0, 72), (x, 72), (255, 255, 255), 1)
+    image = cv2.line(image, (0, 90), (x, 90), (255, 255, 255), 1)
+    image = cv2.line(image, (0, 108), (x, 108), (255, 255, 255), 1)
+
+    ## To draw vertical lines
+    halfx = x // 2
+    l1vStart = (halfx, 0)
+    l1vEnd = (halfx, y)
+
+    quarterx = halfx // 2
+    l2vStart = (quarterx, 0)
+    l2vEnd = (quarterx, y)
+
+    l3vStart = (halfx + quarterx, 0)
+    l3vEnd = (halfx + quarterx, y)
+
+    image = cv2.line(image, (18, 0), (18, y), (255, 255, 255), 1)
+    image = cv2.line(image, (35, 0), (35, y), (255, 255, 255), 1)
+    image = cv2.line(image, (53, 0), (53, y), (255, 255, 255), 1)
+    image = cv2.line(image, (72, 0), (72, y), (255, 255, 255), 1)
+    image = cv2.line(image, (90, 0), (90, y), (255, 255, 255), 1)
+    image = cv2.line(image, (108, 0), (108, y), (255, 255, 255), 1)
 
 
-img1 = cv2.imread('NumberData/Number3/num14.jpg')
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    ## Get Intersection points
+    intersectionPoints = img1 & image
+    count = 0
 
+    for i in range(intersectionPoints.shape[0]):
+        for j in range(intersectionPoints.shape[1]):
+            if intersectionPoints[i][j] > 250:
 
-x = img1.shape[0]
-y = img1.shape[1]
+                count += 1
+    return count
 
-## To draw horizontal lines
-halfy = y // 2
+current_num = 7
 
-l1Start = (0, halfy)
-l1End = (x, halfy)
+num1 = calculate_intersections(f"NumberData/Number{current_num}/num1.jpg")
+num2 = calculate_intersections(f"NumberData/Number{current_num}/num2.jpg")
+num4 = calculate_intersections(f"NumberData/Number{current_num}/num4.jpg")
+num6 = calculate_intersections(f"NumberData/Number{current_num}/num6.jpg")
+num9 = calculate_intersections(f"NumberData/Number{current_num}/num9.jpg")
+num11 = calculate_intersections(f"NumberData/Number{current_num}/num11.jpg")
+num14 = calculate_intersections(f"NumberData/Number{current_num}/num14.jpg")
 
-quartery = halfy // 2
-l2Start = (0, quartery)
-l2End = (x, quartery)
+total = num1 + num2 + num4 + num6 + num9 + num11 + num14
 
-l3Start = (0, halfy + quartery)
-l3End = (x, halfy + quartery)
-print(f"X: {x}, Y: {y}")
-print(l1Start)
-print(l2Start)
-print(l3Start)
+avg = total // 7
+print(avg)
 
-image = np.zeros_like(img1, dtype = np.uint8)
+###
+# 1: 14
+# 2: 22
+# 3: 33
+# 4: 22
+# 5: 27
+# 6: 30
+# 7: 21
+#
 
-image = cv2.line(image, (0, 18), (x, 18), (255, 255, 255), 1)
-image = cv2.line(image, (0, 35), (x, 35), (255, 255, 255), 1)
-image = cv2.line(image, (0, 53), (x, 53), (255, 255, 255), 1)
-image = cv2.line(image, (0, 72), (x, 72), (255, 255, 255), 1)
-image = cv2.line(image, (0, 90), (x, 90), (255, 255, 255), 1)
-image = cv2.line(image, (0, 108), (x, 108), (255, 255, 255), 1)
-
-## To draw vertical lines
-halfx = x // 2
-l1vStart = (halfx, 0)
-l1vEnd = (halfx, y)
-
-quarterx = halfx // 2
-l2vStart = (quarterx, 0)
-l2vEnd = (quarterx, y)
-
-l3vStart = (halfx + quarterx, 0)
-l3vEnd = (halfx + quarterx, y)
-print("vertical")
-print(l1vStart)
-print(l2vStart)
-print(l3vStart)
-
-image = cv2.line(image, (18, 0), (18, y), (255, 255, 255), 1)
-image = cv2.line(image, (35, 0), (35, y), (255, 255, 255), 1)
-image = cv2.line(image, (53, 0), (53, y), (255, 255, 255), 1)
-image = cv2.line(image, (72, 0), (72, y), (255, 255, 255), 1)
-image = cv2.line(image, (90, 0), (90, y), (255, 255, 255), 1)
-image = cv2.line(image, (108, 0), (108, y), (255, 255, 255), 1)
-
-
-## Get Intersection points
-intersectionPoints = img1 & image
-
-# print(img1.shape)
-count = 0
-
-for i in range(intersectionPoints.shape[0]):
-    for j in range(intersectionPoints.shape[1]):
-        if intersectionPoints[i][j] > 250:
-
-            count += 1
-
-
-print(count)
+###
