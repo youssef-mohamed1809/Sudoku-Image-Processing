0d11b561807ec0c5110b274919dddb8d0ac384b8
Youssef <youssef.mohamed1809@gmail.com>
2023-12-28 03:43:54 +0200

    Phase 2 OCR

--- intersections.py
+++ intersections.py
@@ -7,7 +7,6 @@ import math
 def center_image(img1):
     #Image Centering
 
-    # hh, ww = img1.shape
     hh = img1.shape[0]
     ww = img1.shape[1]
 
@@ -30,9 +29,8 @@ def center_image(img1):
 def calculate_intersections(path):
     img1 = cv2.imread(path)
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img1 = cv2.threshold(img1, 1, 255, cv2.THRESH_BINARY)
-    img1 = center_image(img1)
-    plt.imshow(img1, cmap='gray')
+    img1 = cv2.threshold(img1, 110, 255, cv2.THRESH_BINARY)
+    img1 = center_image(img1[1])
 
     x = img1.shape[0]
     y = img1.shape[1]
@@ -42,9 +40,13 @@ def calculate_intersections(path):
     horizotal_lines = np.zeros_like(img1, dtype = np.uint8)
 
     horizotal_lines = cv2.line(horizotal_lines, (0, 28), (x, 28), (255, 255, 255), 1)
+    horizotal_lines = cv2.line(horizotal_lines, (0, 37), (x, 37), (255, 255, 255), 1)
     horizotal_lines = cv2.line(horizotal_lines, (0, 42), (x, 42), (255, 255, 255), 1)
+    horizotal_lines = cv2.line(horizotal_lines, (0, 48), (x, 48), (255, 255, 255), 1)
     horizotal_lines = cv2.line(horizotal_lines, (0, 55), (x, 55), (255, 255, 255), 1)
+    horizotal_lines = cv2.line(horizotal_lines, (0, 62), (x, 62), (255, 255, 255), 1)
     horizotal_lines = cv2.line(horizotal_lines, (0, 69), (x, 69), (255, 255, 255), 1)
+    horizotal_lines = cv2.line(horizotal_lines, (0, 75), (x, 75), (255, 255, 255), 1)
     horizotal_lines = cv2.line(horizotal_lines, (0, 82), (x, 82), (255, 255, 255), 1)
 
     ## To draw vertical lines
@@ -52,9 +54,13 @@ def calculate_intersections(path):
     vertical_lines = np.zeros_like(img1, dtype = np.uint8)
 
     vertical_lines = cv2.line(vertical_lines, (28, 0), (28, y), (255, 255, 255), 1)
+    vertical_lines = cv2.line(vertical_lines, (37, 0), (37, y), (255, 255, 255), 1)
     vertical_lines = cv2.line(vertical_lines, (42, 0), (42, y), (255, 255, 255), 1)
+    vertical_lines = cv2.line(vertical_lines, (48, 0), (48, y), (255, 255, 255), 1)
     vertical_lines = cv2.line(vertical_lines, (55, 0), (55, y), (255, 255, 255), 1)
+    vertical_lines = cv2.line(vertical_lines, (62, 0), (62, y), (255, 255, 255), 1)
     vertical_lines = cv2.line(vertical_lines, (69, 0), (69, y), (255, 255, 255), 1)
+    vertical_lines = cv2.line(vertical_lines, (75, 0), (75, y), (255, 255, 255), 1)
     vertical_lines = cv2.line(vertical_lines, (82, 0), (82, y), (255, 255, 255), 1)
 
 
@@ -65,6 +71,8 @@ def calculate_intersections(path):
     horizontal_count = 0
     vertical_count = 0
 
+    new_img = horizotal_lines + vertical_lines + img1
+    plt.imshow(new_img, cmap='gray')
     for i in range(img1.shape[0]):
         for j in range(img1.shape[1]):
             if horizontal_intersections[i][j] > 230:
@@ -73,74 +81,38 @@ def calculate_intersections(path):
                 vertical_count += 1
     return horizontal_count, vertical_count
 
-current_num = 5
+current_num = 9
 
 num1h, num1v = calculate_intersections(f"NumberData/Number{current_num}/num1.jpg")
 num2h, num2v = calculate_intersections(f"NumberData/Number{current_num}/num2.jpg")
+num3h, num3v = calculate_intersections(f"NumberData/Number{current_num}/num3.jpg")
 num4h, num4v = calculate_intersections(f"NumberData/Number{current_num}/num4.jpg")
+num5h, num5v = calculate_intersections(f"NumberData/Number{current_num}/num5.jpg")
 num6h, num6v = calculate_intersections(f"NumberData/Number{current_num}/num6.jpg")
 num9h, num9v = calculate_intersections(f"NumberData/Number{current_num}/num9.jpg")
+num10h, num10v = calculate_intersections(f"NumberData/Number{current_num}/num10.jpg")
 num11h, num11v = calculate_intersections(f"NumberData/Number{current_num}/num11.jpg")
 num14h, num14v = calculate_intersections(f"NumberData/Number{current_num}/num14.jpg")
 
-total_horizontal =   num1h  + num2h+ num4h +num6h+ num9h + num11h + num14h 
-total_vertical =     num1v + num2v+ num4v + num9v + num11v + num6v + num14h
+total_horizontal = num1h + num2h + num3h + num4h + num5h + num6h + num9h + num10h + num11h + num14h
+total_vertical = num1v + num2v + num3v + num4v + num5v + num6v + num9v + num10v + num11v + num14v
 
-avg_horizontal = total_horizontal // 7
-avg_vertical = total_vertical // 7
+avg_horizontal = total_horizontal // 10
+avg_vertical = total_vertical // 10
 print(f"Average Horizontal Lines: {avg_horizontal}")
 print(f"Average Vertical Lines: {avg_vertical}")
 
 
 ###
-# 1: 11, 16      20 , 20
-# 2: 22, 26      26 , 26
-# 3: 22 ,28      28 , 28
-# 4: 17,21       21 , 21
-# 5: 22,28       28 , 28
-# 6: 17,23       23 , 23
-# 7: 22,21       21 , 21
-# 8: 25,30       30 , 30
-# 9: 20,21       21 , 21
+
+# 1: 23, 16
+# 2: 43, 39
+# 3: 40, 45
+# 4: 35, 36
+# 5: 39, 38
+# 6: 38, 45
+# 7: 24, 28
+# 8: 51, 44
+# 9: 
 
 ###
-
-
-###
-# El a5eera please
-
-# 1: 3, 8
-# 2: 22, 19
-# 3: 13, 17 done
-# 4: 11, 10 lesa
-# 5: 18, 16
-# 6: 30, 27
-# 7: 16, 16
-# 8: 63, 65
-# 9: 34, 31
-###
-
-
-###
-# Number 1: h: 5, v: 1
-# Number 2: h: 9, v: 6
-# Number 3: h: 8, v: 5
-# Number 4: h: 8, v: 9
-# Number 5: h: 12, v: 7
-# Number 6: h: 11, v: 8
-# Number 7: h: 6, v: 6
-# Number 8: h: 11, v: 6
-# Number 9: h: 9, v: 6 
-###
-
-
-# 1: 11, 15
-# 3: 16, 19
-# 4: 18, 21
-# 5: 21, 24
-# 6: 22, 24
-# 7: 12, 18
-# 8: 23, 25
-# 9: 16, 19
-# 2: 15, 17
-
